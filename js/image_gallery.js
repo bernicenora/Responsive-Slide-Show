020be6189b0e272533ed058b7f0bebdf0b58da04
BE <bernicewebdesigner@gmail.com>
2019-11-28 05:39:17 -0700

    Automatic Slide Show/Image Slider - Working

--- js/image_gallery.js
+++ js/image_gallery.js
@@ -1,51 +1,25 @@
 const images = document.getElementsByClassName("thumbnail");
 const selected = document.getElementById("selected");
-const imagelist = images.thumbnail.getElementsByTagName("img");
+const imagelist = images.thumbnail.getElementsByTagName("img");//to access src of img tag
+//Get the buttons
+const left=document.getElementById("left");
+const right=document.getElementById("right");
 
-const opacity = 0.5;
 var count=0;
 
 showSlides();
-
+console.log("working");
 function showSlides(){
     var i;
-    for(i=0;i<images.length;i++){
-        images[i].style.display="none";
+
+    for(i=0;i<imagelist.length;i++){
+        imagelist[i].style.opacity = 1; //to reset opacity
+        selected.src = imagelist[i].src;
     }
     count++;
-    if (count>images.length){count = 1;}
-    images[count-1].style.display="grid";
-    setTimeout(showSlides,5000);
+    if (count>imagelist.length){count = 1;}
+    imagelist[count-1].style.opacity = 0.5; // to set opacity of selected element
+    selected.src = imagelist[count-1].src;
+    setTimeout(showSlides,3000);
 }
 
-//window.onload = function(){
-    //console.log(imagelist.length-1);
-   //for(i=0;i<imagelist.length;i++){
-    //console.log(imagelist[i].src);
-        //console.log(selected.src);
-        //console.log(imagelist[i].src);
-        //console.log(i);
-        //selected.src = imagelist[i].src;
-        //console.log(selected.src);
-        //console.log(imagelist[i].src);
-        //setTimeout(function(){selected.src = imagelist[i].src;},5000);
-        /*if (selected.src != imagelist[i].src){
-        count = count + 1;
-        console.log(imagelist[i].src);
-    }*/
-    //}
-
-
-//}
-
-/*function zoomImg(e){
-    for(i=0;i<images.length;i++){
-       //console.log(selected.src);
-       //console.log(e.target.src);
-
-
-        // Remove fadeIn class after animation time elapsed
-        //setTimeout(function(){selected.classList.remove("fade-in");},600);
-
-    }
-}*/
